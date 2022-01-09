15ee5a632408a43d92d1a6cc85899db35d9fdd7e
kuruzix <75392290+foudreIOX@users.noreply.github.com>
2022-01-09 20:15:21 +0100

    Add files via upload

---
+++ src/foudreIOX/Events/JoinEvent.php
@@ -0,0 +1,36 @@
+﻿<?php
+
+namespace foudreIOX\Events;
+
+use foudreIOX\Main;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\item\ItemFactory;
+use pocketmine\item\ItemIds;
+use pocketmine\Server;
+
+class JoinEvent implements Listener
+{
+    public function onJoin(PlayerJoinEvent $event)
+    {
+        $event->setJoinMessage("");
+
+        $player = $event->getPlayer();
+        $player->getHungerManager()->setFood($player->getHungerManager()->getMaxFood());
+
+
+        if ($event->getPlayer()->hasPlayedBefore()) {
+            Server::getInstance()->broadcastMessage("§f[§eJoinMessage§f] §a[+] §f {$event->getPlayer()->getName()} ");
+        } else {
+            $event->getPlayer()->getInventory()->addItem(ItemFactory::getInstance()->get(ItemIds::WOOD, 0, 64)->setCustomName("§fKIT de base"));
+            Server::getInstance()->broadcastMessage("Bienveunue aux joueur §C {$event->getPlayer()->getName()} §f qui est nouveau sur le serveur");
+        }
+    }
+
+    public function onQuit(PlayerQuitEvent $event)
+    {
+        $event->setQuitMessage("");
+        Server::getInstance()->broadcastMessage("§f[§eQuitMessage§f] §4[-] §f {$event->getPlayer()->getName()}");
+    }
+}
---
+++ src/foudreIOX/Main.php
@@ -0,0 +1,16 @@
+﻿<?php
+
+namespace foudreIOX;
+
+use foudreIOX\Events\JoinEvent;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase
+{
+    public static string $prefix = "§f[§eJoinMesaage§f]";
+    public function onEnable(): void
+    {
+        $this->getLogger()->info("plugin JoinMessageFR est activer");
+        $this->getServer()->getPluginManager()->registerEvents(new JoinEvent(), $this);
+    }
+}
